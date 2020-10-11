3d1b24373673d0d0b61e46624f298bcf836a79f4
facundo <facundoaranaloberia@gmail.com>
2020-10-11 20:34:34 -0300

    agregado interfaz Product + pipes y directivas

---
+++ src/app/product-list/Product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    name: String; 
+    material: String;
+    price: number;
+    stock: number;
+    image: String;
+}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product-list',
@@ -7,13 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  product = {
-    "name": "mueble",
-    "material": "madera",
-    "price": 700,
-    "stock": 1,
-    "image": "assets/img/mueble.png"
-  }
+  products: Product[] = [
+    {
+      name: "estante",
+      material: "madera",
+      price: 700,
+      stock: 1,
+      image: "assets/img/mueble.png",
+    },
+    {
+      name: "sillones",
+      material: "neumaticos",
+      price: 700,
+      stock: 1,
+      image: "assets/img/silla.jpg",
+    },
+    {
+      name: "bolsa",
+      material: "tela",
+      price: 0,
+      stock: 1,
+      image: "assets/img/bolsa.jpg",
+    },
+
+  ]
 
   constructor() { }
 
